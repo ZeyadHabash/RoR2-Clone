157fd9fda317429a36f556d3a5fe51f82391c29c
Zeyad Habash <zeyad.habashy02@gmail.com>
2025-10-31 09:03:10 +0200

    add plugins

---
+++ Assets/Plugins/Feel/MMTools/Core/MMAttributes/MMHiddenAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MoreMountains.Tools
+{
+    public class MMHiddenAttribute : PropertyAttribute
+    {
+    }
+}
---
+++ Assets/Plugins/Feel/MMTools/Core/MMAttributes/MMMonoBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoreMountains.Tools
+{
+    public class MMMonoBehaviour : MonoBehaviour
+    {
+    }
+}
